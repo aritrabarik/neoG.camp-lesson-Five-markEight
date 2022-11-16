5aa3e0474e6166dae0227a0fc6a108e3d56598bc
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-11-16 19:21:02 +0530

    added emojis to dictionary

--- emoji-interpreter/src/App.js
+++ emoji-interpreter/src/App.js
@@ -2,54 +2,58 @@ import React, { useState } from "react";
 import "./App.css";
 
 const emojiDictionary = {
-  "😊": "smiling",
-  "😳": "disbelief",
-  "😔": "sad",
-  "🥡": "takeout box",
-  "❤️": "love",
-  "😑": "annoyance",
+    "😊": "smiling",
+    "😳": "disbelief",
+    "😔": "sad",
+    "🥡": "takeout box",
+    "❤️": "love",
+    "😑": "annoyance",
+    "🤔": "thinking",
+    "🥳": "partying",
+    "😉": "winking",
+    "🤗": "hugging",
 
-  /** add some more to show how the app now expands when there's new data */
+    /** add some more to show how the app now expands when there's new data */
 };
 
 var emojis = Object.keys(emojiDictionary);
 
 function App() {
-  const [emoji, setEmoji] = useState("");
-  const [meaning, setMeaning] = useState("translation will appear here...");
+    const [emoji, setEmoji] = useState("");
+    const [meaning, setMeaning] = useState("translation will appear here...");
 
-  function emojiInputChangeHandler(event) {
-    var inputEmoji = event.target.value;
-    setEmoji(inputEmoji);
+    function emojiInputChangeHandler(event) {
+        var inputEmoji = event.target.value;
+        setEmoji(inputEmoji);
 
-    if (inputEmoji in emojiDictionary) {
-      setMeaning(emojiDictionary[inputEmoji]);
-    } else {
-      setMeaning("the given emoji is not in our database!");
+        if (inputEmoji in emojiDictionary) {
+            setMeaning(emojiDictionary[inputEmoji]);
+        } else {
+            setMeaning("the given emoji is not in our database!");
+        }
     }
-  }
 
-  function emojiClickHanler(item) {
-    setMeaning(emojiDictionary[item]);
-  }
+    function emojiClickHanler(item) {
+        setMeaning(emojiDictionary[item]);
+    }
 
-  return (
-    <div className="App">
-      <h1>emojipediaaaaa</h1>
-      <input
-        onChange={emojiInputChangeHandler}
-        value={emoji}
-        placeholder="enter emoji you want meaning of"
-      ></input>
-      <h2>{emoji}</h2>
-      <h3>{meaning}</h3>
-      {emojis.map((item) => (
-        <span key={item} onClick={() => emojiClickHanler(item)}>
-          {item}
-        </span>
-      ))}
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>emojipediaaaaa</h1>
+            <input
+                onChange={emojiInputChangeHandler}
+                value={emoji}
+                placeholder="enter emoji you want meaning of"
+            ></input>
+            <h2>{emoji}</h2>
+            <h3>{meaning}</h3>
+            {emojis.map((item) => (
+                <span key={item} onClick={() => emojiClickHanler(item)}>
+                    {item}
+                </span>
+            ))}
+        </div>
+    );
 }
 
 export default App;
